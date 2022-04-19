24999a93b3d3db1c281886f3bb641cad69dba3bc
Siam Islam <99339320+siam600@users.noreply.github.com>
2022-04-19 10:28:40 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:false,
+  autoplay:true,
+  autoplayTimeout:2000,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:1
+      }
+  }
+})
